816a74995395d044a3f1865b7ff1283781b12ba4
Sahraabdi23 <101500212+Sahraabdi23@users.noreply.github.com>
2023-05-12 23:51:31 +0300

    Update settings.py

--- todo_list/settings.py
+++ todo_list/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'd)=^c7!0-oqjmqve%(bt+p#sq6x*ipz2keh741j*-@f@_)f!1t'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = False
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ["venv.eu-north-1.elasticbeanstalk.com"]
 
 
 # Application definition
